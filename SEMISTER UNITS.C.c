826b343f0f09ff7c37b75999d74e3000158c185f
SHIKUJAMES <mugomary2004@gmail.com>
2025-09-24 09:33:07 +0300

    Add files via upload

---
+++ SEMISTER UNITS.C.c
@@ -0,0 +1,20 @@
+//
+/*
+Name:Mary WANJIKU
+Reg:CT/100/26211/25
+Description:semester units
+*/
+#include<stdio.h>
+
+int main()
+{
+    printf("Mathematics for science \n");
+    printf("Computer system and organisation \n");
+    printf("Communication skills \n");
+    printf("Introduction to computer application \n");
+    printf("Essentials of accounting \n");
+    printf("Fundamentals of ICT \n");
+    printf("Analogue electronics \n");
+    printf("Introduction to programming and algorithm \n");
+    return 0;
+}
